a627d12808e8dba4737f66489c6e6b7170ffe3d5
nyrkln <nayraksv@estudante.ufscar.br>
2022-08-29 20:17:18 -0300

    correção leitorDeBanco

--- Teste.py
+++ Teste.py
@@ -6,37 +6,34 @@ import datetime
 import os
 ListaDeObras = [] 
 def leitorDeBanco(Lista):
-
-    with open(os.path.join("BD","Banco.txt"), "r") as rf:
-        proximo = str(rf.readline())
-        while(proximo != '-5\n'):
-            nome = proximo
-            editora = str(rf.readline())
-            isbn = int(rf.readline())
-            autor = str(rf.readline())
-            palavras_chave = str(rf.readline()).split(",")
-            data_publi = datetime.datetime.strptime(str(rf.readline()),'%Y-%d-%m\n')
-            nro_paginas = int(rf.readline())
-            categoria_obra = int(rf.readline())
-            copias = []
+    rf = open(os.path.join("BD","Banco copy.txt"),"r")
+    proximo = str(rf.readline())
+    while(proximo != '-5'):
+        nome = proximo
+        editora = str(rf.readline())
+        isbn = int(rf.readline())
+        autor = str(rf.readline())
+        palavras_chave = str(rf.readline()).split(",")
+        data_publi = datetime.datetime.strptime(str(rf.readline()),"%Y-%d-%m\n")
+        nro_paginas = int(rf.readline())
+        categoria_obra = int(rf.readline())
+        copias = []
+        proximo = rf.readline()
+        sinal = True
+        while(sinal and str(proximo) != '-1\n'):
+            base = str(proximo).split(",")
+            copiaBase = CopiaObra(int(base[0]),int(base[1]))
+            copias.append(copiaBase)
             proximo = rf.readline()
-            sinal = True
-            while(sinal and str(proximo) != '-1\n'):
-                base = str(proximo).split(",")
-                copiaBase = CopiaObra(int(base[0]),int(base[1]))
-                copias.append(copiaBase)
-                proximo = rf.readline()
-                if (str(proximo) == '-1\n'):
-                    sinal = False
-            cadastro = Obra(nome,editora,isbn,autor,palavras_chave,data_publi,nro_paginas,categoria_obra,copias)    
-            Lista.append(cadastro)
-            proximo = rf.readline()
+            if (str(proximo) == '-1\n'):
+                sinal = False
+        cadastro = Obra(nome,editora,isbn,autor,palavras_chave,data_publi,nro_paginas,categoria_obra,copias)    
+        Lista.append(cadastro)
+        proximo = rf.readline()
+    rf.close()
 
 leitorDeBanco(ListaDeObras)
 
-#print(ListaDeObras[0])
-
-#date = datetime.date(2013, 1, 1)
 copia1Lobo = CopiaObra(1,1)
 copia2Lobo = CopiaObra(2,2)
 copia3Lobo = CopiaObra(3,3)
