b4d46f7df121306285e3005c1eaf1b377ae7a47d
ThePaladini <othepaladini@gmail.com>
2022-09-18 20:44:59 -0300

    implementação final de pessoas

--- pooa/app/pessoas/use_cases_concrete.py
+++ pooa/app/pessoas/use_cases_concrete.py
@@ -6,6 +6,66 @@ from pooa.app.pessoas.use_cases_interfaces import (
     IConsultarGruposAcademicosUseCase
 )
 
+def reescreve_bd(ListaDePessoas):
+    with open(os.path.join("BD","BancoPessoa.txt"), "w") as af:
+        pass
+    with open(os.path.join("BD","BancoPessoa.txt"), "a+") as af:
+        for pessoa in ListaDePessoas[0]:
+            af.write(pessoa.nome)
+            af.write('\n')
+            af.write(str(pessoa.cpf))
+            af.write('\n')
+            af.write(str(pessoa.cep))
+            af.write('\n')
+            af.write(str(pessoa.data_nasc))
+            af.write('\n')
+            af.write(str(pessoa.telefone))
+            af.write('\n')
+            af.write(str(pessoa.email))
+            af.write('\n')
+            af.write(str(pessoa.usuario.value))
+            af.write('\n')
+            af.write(str(pessoa.senha))
+            af.write('\n')
+            if pessoa.usuario.value == 2:
+                af.write(str(pessoa.identificacao))
+                af.write('\n')
+            elif pessoa.usuario.value == 3:
+                af.write(str(pessoa.grupoAcademico))
+                af.write('\n')
+                af.write(str(pessoa.idGrupoAcademico))
+                af.write('\n')
+            af.write('-1')
+            af.write('\n')
+        for pessoa in ListaDePessoas[1]:
+            af.write(pessoa.nome)
+            af.write('\n')
+            af.write(str(pessoa.cpf))
+            af.write('\n')
+            af.write(str(pessoa.cep))
+            af.write('\n')
+            af.write(str(pessoa.data_nasc))
+            af.write('\n')
+            af.write(str(pessoa.telefone))
+            af.write('\n')
+            af.write(str(pessoa.email))
+            af.write('\n')
+            af.write(str(pessoa.usuario.value))
+            af.write('\n')
+            af.write(str(pessoa.senha))
+            af.write('\n')
+            if pessoa.usuario.value == 2:
+                af.write(str(pessoa.identificacao))
+                af.write('\n')
+            elif pessoa.usuario.value == 3:
+                af.write(str(pessoa.grupoAcademico))
+                af.write('\n')
+                af.write(str(pessoa.idGrupoAcademico))
+                af.write('\n')
+            af.write('-1')
+            af.write('\n')
+        af.write('-5') 
+
 class AdicionarUsuarioUseCase(IAdicionarUsuarioUseCase):
     def adicionarUsuario(futuraListaDePessoas,pessoa):
         conteudo = []
@@ -33,8 +93,8 @@ class AdicionarUsuarioUseCase(IAdicionarUsuarioUseCase):
                 stripped_line = line.strip()
                 new_line = stripped_line.replace("-5", str(Id))
                 new_file_content += new_line +"\n"
-                f.close() #NECESSARIO?
-        writing_file = open(os.path.join("BD","BancoPessoa.txt"), "w")  #CODIGO EXEMPLO??
+                f.close() 
+        writing_file = open(os.path.join("BD","BancoPessoa.txt"), "w") 
         writing_file.write(new_file_content)
         writing_file.close()
 
@@ -82,9 +142,15 @@ class ConsultarGruposAcademicosUseCase(IConsultarGruposAcademicosUseCase):
 
 class AlterarDadosUsuarioUseCase(IAlterarDadosUsuarioUseCase):
     def alterarDadosUsuario(self,usuario,futuraListaDeUsuarios):
-        for indice,usuarios in enumerate(futuraListaDeUsuarios):
+        for indice,usuarios in enumerate(futuraListaDeUsuarios[0]):
             if usuario.cpf == usuarios.cpf:
-                futuraListaDeUsuarios[indice] = usuario
+                futuraListaDeUsuarios[0][indice] = usuario
+                reescreve_bd(futuraListaDeUsuarios)
                 return True
+        for indice,usuarios in enumerate(futuraListaDeUsuarios[1]):
+            if usuario.cpf == usuarios.cpf:
+                futuraListaDeUsuarios[1][indice] = usuario
+                reescreve_bd(futuraListaDeUsuarios)
+                return True    
         return False
         ...
