1e78ab9da34e1f63ffd3d18a381894faedb6ace9
ThePaladini <othepaladini@gmail.com>
2022-09-19 17:40:28 -0300

    adicionei runtime

---
+++ app.py
@@ -0,0 +1,8 @@
+from distutils.log import debug
+from server.instace import server
+from server.situacao import *
+import os 
+
+port = int(os.environ.get('PORT', 5000)) 
+app.run(host='0.0.0.0', port=port)
+#server.run()
